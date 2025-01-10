554780927cf3218aff6d988af927a37affd85885
jmk445 <jmk445123456@gmail.com>
2025-01-10 15:25:45 +0900

    unsolved : P1380

---
+++ src/main/java/P1380/P1380.java
@@ -0,0 +1,17 @@
+package P1380;
+
+import java.util.Scanner;
+
+public class P1380 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        while(sc.nextInt() != 0){
+            String[] arr = new String[N];
+            for(int i = 0; i < N; i++){
+                arr[i] = sc.next();
+            }
+        }
+
+    }
+}
